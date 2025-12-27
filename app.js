bd7e322cecfef048f46a050a0e220413e05d3f35
Deep787721 <dk02435449@gmail.com>
2025-12-27 09:30:29 +0530

    add new form

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-// add new feature
+// add new feature - form
+
+console.log("Form feature added");
+function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    console.log("Form submitted with data:", Object.fromEntries(formData.entries()));
+}
